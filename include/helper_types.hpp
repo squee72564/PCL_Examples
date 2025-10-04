a6b76491dd7ab100b51827acc227178b82a9f239
Alex Rodriguez <rodriguez.al183@gmail.com>
2025-10-04 08:00:24 -0700

    Add comment explaining variant type

--- include/helper_types.hpp
+++ include/helper_types.hpp
@@ -5,9 +5,20 @@
 
 #include <pcl/point_types.h>
 #include <pcl/point_cloud.h>
-#include <pcl/sample_consensus/sac.h>
-#include <pcl/sample_consensus/ransac.h>
 
+/*
+*   This variant is used to represent multiple possible
+*   specializations for pcl::PointCloud<PointT>
+*
+*   Using a variant like this we can use 
+*   pcl::PCLPointCloud2 to agnostically read from a
+*   .pcd file and then just metadata from the file to 
+*   convert to one of the types within this variant.
+*
+*   using std::visit we can get access to the actual type 
+*   held within the variant and apply different algorithms 
+*   from PCL on this type
+*/
 using PointCloudVariantPtr = std::variant<
         pcl::PointCloud<pcl::PointXYZRGBA>::Ptr,
         pcl::PointCloud<pcl::PointXYZRGB>::Ptr,
