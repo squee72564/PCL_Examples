a345abb0a5f17cdb4a4952c0822963704357235c
Alex Rodriguez <rodriguez.al183@gmail.com>
2025-10-03 23:11:59 -0700

    Delete unneeded traits
    I didn't realize that the PCL classes actually have a public definition for the point type

--- include/helpers.hpp
+++ include/helpers.hpp
@@ -12,18 +12,6 @@
 template<class T>
 inline constexpr bool always_false_v = false;
 
-// Trait to extract PointT form pcl::PointCloud<PointT>
-template <typename T>
-struct point_type_of;
-
-template <typename PointT>
-struct point_type_of<pcl::PointCloud<PointT>> {
-    using type = PointT;
-};
-
-template <typename T>
-using point_type_of_t = typename point_type_of<T>::type;
-
 // Function to load a PCD file into PCLPointCloud2
 int loadPCDFileManual(const std::string &file_name,
                       pcl::PCLPointCloud2 &pointCloud, Eigen::Vector4f &origin,
